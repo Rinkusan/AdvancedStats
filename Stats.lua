75c0956ec418b9f6dc72c31ab22d35eb88f692b7
Rinkusan <59068408+Rinkusan@users.noreply.github.com>
2019-12-21 21:32:40 +0100

    Added Information on Spells

--- Stats.lua
+++ Stats.lua
@@ -51,11 +51,11 @@ local function ArcaneRes()
 	base, total, bonus, minus = UnitResistance("player",6)
 	return total;
 end
--- Function to calculate Tankiness Value
+-- Function to calculate Bear Tankiness Value
 local function Tankiness()
 	return math.floor((GetDodgeChance("player")*16.67) + (Armor()*0.23) + Stam() + (Agi()*0.92) + (Defense()*2));
 end
--- Function to calculate Threat Value
+-- Function to calculate Bear Threat Value
 local function Threat()
 	return math.floor(Str() + (Agi()*0.45) + (AP()*0.5) + (GetCritChance("player")*8.97) + (GetHitModifier("player")*6.69) + (GetHaste("player")*9.01))
 end
@@ -69,13 +69,15 @@ local function Bear()
 	return "Hit Chance: " .. GetHitModifier("player").."\n".."Crit Chance: " .. math.floor(GetCritChance()+0.5).."\n".."Dodge Chance: " .. math.floor(GetDodgeChance()+0.5).."\n".."Attack Power: " .. AP().."\n".."Armor Rating: " .. Armor().."\n".."Defense Rating: " .. Defense().."\n".."Tankiness Value: " .. Tankiness().."\n".."Threat Value: " .. Threat();
 end
 local function Druid()
-	return "Fire Resistance: " .. FireRes().."\n".."Frost Resistance: " .. FrostRes().."\n".."Nature Resistance: " .. NatureRes().."\n".."Shadow Resistance: " .. ShadowRes().."\n".."Arcane Resistance: " .. ArcaneRes();
+	return "Bonus Spell Damage: " .. GetSpellBonusDamage(7).."\n".."Bonus Healing: " .. GetSpellBonusHealing().."\n".."Spell Crit Chance: " .. math.floor(GetSpellCritChance()+0.5).."\n".."Spell Hit Modifier: " .. GetSpellHitModifier().."\n".."Fire Resistance: " .. FireRes().."\n".."Frost Resistance: " .. FrostRes().."\n".."Nature Resistance: " .. NatureRes().."\n".."Shadow Resistance: " .. ShadowRes().."\n".."Arcane Resistance: " .. ArcaneRes();
 end
 local Travel = [[I'm in Travel Form]]
 local function Cat()
 	return "Hit Chance: " .. GetHitModifier("player").."\n".."Crit Chance: " .. math.floor(GetCritChance()+0.5).."\n".."Attack Power: " .. AP();
 end
-local Boomkin = [[I'm an Owl! Boom Boom!]]
+local function Boomkin()
+	return "Bonus Spell Damage: " .. GetSpellBonusDamage(7).."\n".."Bonus Healing: " .. GetSpellBonusHealing().."\n".."Spell Crit Chance: " .. math.floor(GetSpellCritChance()+0.5).."\n".."Spell Hit Modifier: " .. GetSpellHitModifier().."\n".."Fire Resistance: " .. FireRes().."\n".."Frost Resistance: " .. FrostRes().."\n".."Nature Resistance: " .. NatureRes().."\n".."Shadow Resistance: " .. ShadowRes().."\n".."Arcane Resistance: " .. ArcaneRes();
+end
 -- define frame and set attributes
 local frame = CreateFrame("Frame", "MyFrame", UIParent)
 	frame:SetMovable(true)
@@ -114,6 +116,7 @@ local function AutoUpdate()
 			elseif GetShapeshiftFormID() == 1 then Display(Cat())
 			elseif GetShapeshiftFormID() == 3 then Display(Travel)
 			elseif GetShapeshiftFormID() == 5 then Display(Bear())
+			elseif GetShapeshiftFormID() == 31 then Display(Boomkin())
 			else Display(Druid())
 			end
 		else Display(Druid())
